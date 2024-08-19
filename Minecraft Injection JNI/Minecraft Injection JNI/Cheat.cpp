146e3d048061fdf84d2c876757f17a8086e51b66
BadlyacX <117196010+BadlyacX@users.noreply.github.com>
2024-08-20 00:09:19 +0800

    Update Cheat.cpp

--- Minecraft Injection JNI/Minecraft Injection JNI/Cheat.cpp
+++ Minecraft Injection JNI/Minecraft Injection JNI/Cheat.cpp
@@ -26,12 +26,70 @@ jobject getPlayer()
 	return ct.env->GetObjectField(getMinecraft(), getPlayerField);
 }
 
+jobject getPlayerConnection()
+{
+	jfieldID playerConnectionField = ct.env->GetFieldID(ct.env->GetObjectClass(getPlayer()), "a", "Lnh;");
+	return ct.env->GetObjectField(getPlayer(), playerConnectionField);
+}
+
+void sendPacket(jobject packet)
+{
+	jclass playerConnectionClass = ct.env->GetObjectClass(getPlayerConnection());
+	jmethodID sendPacketMethod = ct.env->GetMethodID(playerConnectionClass, "a", "(Lff;)V");
+	ct.env->CallVoidMethod(getPlayerConnection(), sendPacketMethod, packet);
+}
+
 void setPlayerSprint()
 {
 	jmethodID setSprintingMethod = ct.env->GetMethodID(ct.env->GetObjectClass(getPlayer()), "d", "(Z)V");
 	ct.env->CallBooleanMethod(getPlayer(), setSprintingMethod, true);
 }
 
+void sendPacketToPlayer(jobject packet)
+{
+	jobject playerConnection = getPlayerConnection();
+	jclass playerConnectionClass = ct.env->GetObjectClass(playerConnection);
+	jmethodID sendPacketMethod = ct.env->GetMethodID(playerConnectionClass, "a", "(Lff;)V");
+	ct.env->CallVoidMethod(playerConnection, sendPacketMethod, packet);
+}
+
+void sendParticleEffectToPlayer(jobject player)
+{
+	jobject particlePacket = createParticlePacket("explode", 0.0f, 100.0f, 0.0f, 0.5f, 0.5f, 0.5f, 0.1f, 10);
+	sendPacketToPlayer(particlePacket);
+}
+
+jobject createParticlePacket(const char* particleName, float x, float y, float z, float offsetX, float offsetY, float offsetZ, float speed, int count)
+{
+	jclass particlePacketClass = ct.env->FindClass("gp");
+	jmethodID particlePacketConstructor = ct.env->GetMethodID(particlePacketClass, "<init>", "(Ljava/lang/String;FFFFFIF)V");
+
+	jstring jParticleName = ct.env->NewStringUTF(particleName);
+	return ct.env->NewObject(particlePacketClass, particlePacketConstructor, jParticleName, x, y, z, offsetX, offsetY, offsetZ, speed, count);
+}
+
+void sendParticleEffect()
+{
+	jobject particlePacket = createParticlePacket("explode", 0.0f, 100.0f, 0.0f, 0.5f, 0.5f, 0.5f, 0.1f, 10);
+	sendPacket(particlePacket);
+}
+
+void detectPlayerJoin(JNIEnv* env) 
+{
+	jobject minecraft = getMinecraftClass();
+	jclass clientPlayNetworkHandlerClass = env->FindClass("cls");
+	jmethodID onGameJoinMethod = env->GetMethodID(clientPlayNetworkHandlerClass, "onGameJoin", "()V");
+
+	if (onGameJoinMethod != NULL) 
+	{
+		env->CallVoidMethod(minecraft, onGameJoinMethod);
+		printf("Player has joined the game!\n");
+	}
+	else 
+	{
+		printf("Failed to find onGameJoin method.\n");
+	}
+}
 
 void runModules()
 {
